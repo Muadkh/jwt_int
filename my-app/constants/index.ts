4b341e4c3fcee4678bc36665a6f5eff16d1c4d3d
Muhammad Adil Khan <115182139+Muadkh@users.noreply.github.com>
2023-08-16 22:58:58 +0500

    Delete my-app directory

--- my-app/constants/index.ts
+++
@@ -1,2 +0,0 @@
-export const COOKIE_NAME= 'Our_JWT_Token'
-export const Mak_AGE= 60*60*24
--- my-app/drizzle/0000_loving_bloodstrike.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS "users" (
-	"user_id" serial PRIMARY KEY NOT NULL,
-	"user_name" text,
-	"user_email_address" text,
-	"user_password" varchar(256)
-);
